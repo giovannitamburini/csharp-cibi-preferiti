ab1e1deaadc5f68ed2eb56cc9531c0402cc3fd00
giovannitamburini <giovatambu@gmail.com>
2023-09-06 10:51:14 +0200

    corretto il nome dell' array

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,38 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // Inizializzo un array con la classifica dei miei cibi preferiti (minimo 5, massimo 10 elementi).
-string[] favorite_foods = { "pizza", "hamburger", "cappelletti", "tagliatelle", "carbonara", "carciofi" };
+string[] FavoriteFoods = { "pizza", "hamburger", "cappelletti", "tagliatelle", "carbonara", "carciofi" };
 
 // stampo a video la lunghezza della classifica
-Console.WriteLine($"la lunghezza della mia classifica è {favorite_foods.Length}");
+Console.WriteLine($"la lunghezza della mia classifica è {FavoriteFoods.Length}");
 
 Console.WriteLine();
 
 // stampo a video la mia classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
 Console.WriteLine("La mia classifica è:");
 
-for (int i = 0; i < favorite_foods.Length; i++)
+for (int i = 0; i < FavoriteFoods.Length; i++)
 {
-    Console.WriteLine((i + 1) + " - " + favorite_foods[i]);
+    Console.WriteLine(i + 1 + " - " + FavoriteFoods[i]);
 }
 
 Console.WriteLine();
 
 // stampo a video il mio cibo top
-Console.WriteLine($"Il mio cibo top è: {favorite_foods[0]}");
+Console.WriteLine($"Il mio cibo top è: {FavoriteFoods[0]}");
 
 Console.WriteLine();
 
 // stampo a video il mio cibo preferito ma non troppo(ultima posizione in classifica)
-Console.WriteLine($"Il mio cibo preferito ma non troppo: {favorite_foods[^1]}");
+Console.WriteLine($"Il mio cibo preferito ma non troppo: {FavoriteFoods[^1]}");
 
 Console.WriteLine();
 
 // BONUS: stampo a schermo il cibo di mezza classifica
 // caso in cui la classifica presenta un numero di elementi pari
-if (favorite_foods.Length % 2 == 0)
+if (FavoriteFoods.Length % 2 == 0)
 {
-    Console.Write($"Gli elementi mediani della classifica sono: {favorite_foods[(favorite_foods.Length - 1) / 2]}, {favorite_foods[(favorite_foods.Length / 2)]}");
+    Console.Write($"Gli elementi mediani della classifica sono: {FavoriteFoods[(FavoriteFoods.Length - 1) / 2]}, {FavoriteFoods[(FavoriteFoods.Length / 2)]}");
 
     /*
     OPPURE
@@ -50,8 +50,7 @@ if (favorite_foods.Length % 2 == 0)
 }
 else
 {
-    Console.Write($"L'elemento mediano della classifica è: {favorite_foods[favorite_foods.Length / 2]}");
-
+    Console.Write($"L'elemento mediano della classifica è: {FavoriteFoods[FavoriteFoods.Length / 2]}");
 }
 
 Console.WriteLine();
